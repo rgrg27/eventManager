bb9d930f1dfe05b12b8bcfd930e06d5ab7cbfb79
Rahul Garg <rgrg27@gmail.com>
2020-08-06 11:39:08 +0530

    Updates

---
+++ precache-manifest.55819e94cf3e9d0a310ed628bc8cda66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e5101a599255f5f5eba4657120becac",
+    "url": "/eventManager/index.html"
+  },
+  {
+    "revision": "47e09c501dbf34ee9eac",
+    "url": "/eventManager/static/css/main.f3e7d21f.chunk.css"
+  },
+  {
+    "revision": "3f60af8d7dcd48fe77db",
+    "url": "/eventManager/static/js/2.2678401a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/eventManager/static/js/2.2678401a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47e09c501dbf34ee9eac",
+    "url": "/eventManager/static/js/main.03f0d7ac.chunk.js"
+  },
+  {
+    "revision": "0d886f7c42666f7cb214",
+    "url": "/eventManager/static/js/runtime-main.00248a67.js"
+  }
+]);
