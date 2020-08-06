327529f22097020ef4cadd46f97af59c0ce46ed5
Rahul Garg <rgrg27@gmail.com>
2020-08-06 17:36:56 +0530

    Updates

--- precache-manifest.01e7c98375e2153030db76ac70887dee.js
+++ precache-manifest.01e7c98375e2153030db76ac70887dee.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1aecc5025fccbeb66b8d49230b0b4a3d",
+    "revision": "7128a8f6dd762dd0dc455613871ae8a8",
     "url": "/eventManager/index.html"
   },
   {
-    "revision": "bf11445138f4d98c453c",
+    "revision": "301dc3520e510bf8f8b1",
     "url": "/eventManager/static/css/main.2b054e3d.chunk.css"
   },
   {
-    "revision": "3f60af8d7dcd48fe77db",
-    "url": "/eventManager/static/js/2.2678401a.chunk.js"
+    "revision": "2dd0d163e03e6cce95b6",
+    "url": "/eventManager/static/js/2.53ef4404.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/eventManager/static/js/2.2678401a.chunk.js.LICENSE.txt"
+    "url": "/eventManager/static/js/2.53ef4404.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bf11445138f4d98c453c",
-    "url": "/eventManager/static/js/main.42b5072c.chunk.js"
+    "revision": "301dc3520e510bf8f8b1",
+    "url": "/eventManager/static/js/main.5768837c.chunk.js"
   },
   {
     "revision": "0d886f7c42666f7cb214",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/eventManager/precache-manifest.7f7ecc201fd6c5db944e75b0237e6382.js"
+  "/eventManager/precache-manifest.01e7c98375e2153030db76ac70887dee.js"
 );
 
 self.addEventListener('message', (event) => {
